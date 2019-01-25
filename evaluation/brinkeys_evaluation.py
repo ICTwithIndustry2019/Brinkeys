43b26d05e09f2ac4c49bac9df36442425c3bc6b7
Sara Veldhoen <SVE020@wpakb.kb.nl>
2019-01-25 11:55:23 +0100

    computation per label

--- evaluation/brinkeys_evaluation.py
+++ evaluation/brinkeys_evaluation.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import csv
+from collections import defaultdict
 
 goldstandard = csv.reader(open('ppn_identifier_brinkies_gold.csv'),delimiter=';')
 
@@ -32,6 +33,8 @@ results = csv.reader(open(resultsLocation),delimiter=';')
 
 precisions = []
 recalls = []
+scores_per_label = defaultdict(lambda: defaultdict(lambda: 0))
+
 
 for row in results:
     # get corresponding row from gold standard
@@ -43,16 +46,24 @@ for row in results:
         rowBrinkeys = list(filter(None, row))
         goldBrinkeys = list(filter(None, goldBrinkeys))
         
-        
+
         # calculate recall at 20
         correctBrinkeys = list(set(goldBrinkeys) & set(row)) # get intersection of two lists of Brinkeys
         rowRecall = len(correctBrinkeys) / len(goldBrinkeys) # recall |correct| / |all relevant|
         #print(f'Recall: {rowRecall}')
-        
+
         # calculate precision at 3
         rowFirstThree = rowBrinkeys[0:len(goldBrinkeys)] # do len of brinkeys and not 3 for when there's less than 3 brinkeys in gold
-        goldFirstThree = goldBrinkeys[0:len(goldBrinkeys)] 
-        
+        goldFirstThree = goldBrinkeys[0:len(goldBrinkeys)]
+
+        for brinkey in goldBrinkeys:
+            scores_per_label[brinkey]['occur']+= 1
+            scores_per_label[brinkey]['truePos@3']+= int(brinkey in rowFirstThree)
+            scores_per_label[brinkey]['truePos@20']+= int(brinkey in rowBrinkeys)
+            scores_per_label[brinkey]['falseNeg@20']+= int(brinkey not in rowBrinkeys)
+        for brinkey in rowBrinkeys:
+            scores_per_label[brinkey]['falsePos@3']+= int(brinkey not in goldBrinkeys)
+
         #print(f'row : {rowFirstThree}')
         #print(f'gold: {goldFirstThree}')
         
@@ -70,7 +81,7 @@ for row in results:
     else:
         print(f'Key "{row[0]}" not found in gold standard')
 
-overallRecall = sum(recalls)/len(recalls)    
+overallRecall = sum(recalls)/len(recalls)
 overallPrecision = sum(precisions)/len(precisions) 
 f1 = 2 * ((overallPrecision * overallRecall) / (overallPrecision + overallRecall))
 
@@ -78,5 +89,19 @@ print(f'Precision at 3: {overallPrecision}')
 print(f'Recall at 20  : {overallRecall}')
 print(f'F1            : {f1}')
 
+for label, metrics in scores_per_label.items():
+    if metrics['occur']<1: continue  #undefined
+    if metrics['occur']<10: continue # only measure meaningfull labels
+    for k,v in metrics.items():
+       print(k,v)
+
+    try:
+       precision  = metrics['truePos@3']  / (metrics['truePos@3'] + metrics['falsePos@3'])
+    except:
+       precision = 0
+    print( metrics['truePos@20'], metrics['truePos@20'] , metrics['falseNeg@20'] )
+
+    recall  = metrics['truePos@20'] / ( metrics['truePos@20'] + metrics['falseNeg@20'] )
+    print('\t'+label+' - Precision: '+str(precision)+', recall: '+str(recall))
 
 
